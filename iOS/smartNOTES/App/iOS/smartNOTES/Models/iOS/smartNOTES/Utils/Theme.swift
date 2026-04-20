2e68efdbd9c3b94e7533b204c3bb74d63ba5f02e
aissatasidibe722-create <aissatasidibe722@gmail.com>
2026-04-20 12:48:10 +0000

    Create Theme.swift

---
+++ iOS/smartNOTES/App/iOS/smartNOTES/Models/iOS/smartNOTES/Utils/Theme.swift
@@ -0,0 +1,9 @@
+import SwiftUI
+
+struct AppTheme {
+    static let primaryBlue = Color(red: 0.2, green: 0.4, blue: 1.0)
+    static let accentPurple = Color(red: 0.7, green: 0.3, blue: 1.0)
+    static let background = Color(UIColor.systemBackground)
+    static let titleFont = Font.system(size: 28, weight: .bold)
+    static let bodyFont = Font.system(size: 16, weight: .regular)
+}
