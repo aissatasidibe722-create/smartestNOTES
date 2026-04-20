45a69646c14dfaa20beb5899b627a5ff01eb3c1b
aissatasidibe722-create <aissatasidibe722@gmail.com>
2026-04-20 12:48:31 +0000

    Create Constants.swift

---
+++ iOS/smartNOTES/App/iOS/smartNOTES/Models/iOS/smartNOTES/Utils/iOS/smartNOTES/Utils/Constants.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+struct Constants {
+    struct Strings {
+        static let appName = "smartNOTES"
+        static let myNotes = "Mes notes"
+        static let newNote = "Nouvelle note"
+    }
+}
